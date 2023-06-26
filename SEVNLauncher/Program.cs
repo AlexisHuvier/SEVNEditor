b9f1fe3a8fba7d9903fe56374f6c64e819dfe5f3
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-06-26 14:03:58 +0200

    feat(launcher): Create SEVNLauncher

---
+++ SEVNLauncher/Program.cs
@@ -0,0 +1,14 @@
+﻿using SEVNEditor;
+using SharpEngine;
+
+namespace SEVNLauncher;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var win = new Window(1200, 800, "Sharp Engine Visual Novel Launcher", Constants.BgColor, true);
+
+        win.Run();
+    }
+}
